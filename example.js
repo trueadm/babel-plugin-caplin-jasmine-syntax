50644858b02a48343cd613d848335855a7cef18c
Dominic Gannaway <dominic.gannaway@caplin.com>
2016-03-16 12:33:46 +0000

    Better validates arguments where edge cases exist

--- example.js
+++ example.js
@@ -4,17 +4,76 @@ var babel  = require("babel-core");
 var plugin = require("./lib/index");
 
 var code = `
-	it('do something', function() {
-		waits(1000);
-		waitsFor(function() {
-			value++;
-			return flag;
-		}, "The Value should be incremented", 750);
+var sinon = require("sinon");
+
+var AppRouter = require("router/AppRouter");
+var DummyRouteConfig = require("router/DummyConfig").dummyRouteConfig;
+var DummyHandlers = require("router/DummyConfig").dummyHandlers;
+
+describe("Test the AppRouter", function () {
+	var sandbox = null;
+	var routerGoSpy = null;
+
+	beforeEach(function () {
+		sandbox = sinon.sandbox.create();
+		routerGoSpy = sandbox.spy(AppRouter, "go");
+		AppRouter.config(DummyRouteConfig);
+
+	});
+
+	afterEach(function () {
+		sandbox.restore();
+	});
+
+	it("should trigger the correct entering callbacks when going to the first route.", function() {
+		AppRouter.go("/foo/bar");
+
+		waits(100);
+
 		runs(function() {
-			doStuff()
-			expect(foo).toBe(bar);
+			sinon.assert.calledOnce(DummyHandlers.indexHandler.enter);
+			sinon.assert.calledOnce(DummyHandlers.mainSectionHandler.enter);
+			sinon.assert.calledOnce(DummyHandlers.subSectionHandler.enter);
 		});
 	});
+
+	// Going from "/foo/bar" to  "buzz/bizz"
+	it("should trigger the correct exiting, updating and entering callbacks when changing route", function() {
+		DummyHandlers.indexHandler.updateSettings.returns(false);
+		DummyHandlers.mainSectionHandler.updateSettings.returns(true);
+		DummyHandlers.subSectionHandler.updateSettings.returns(true);
+
+		AppRouter.go("/buzz/bizz");
+
+		waits(100);
+
+		runs(function() {
+			sinon.assert.called(DummyHandlers.subSectionHandler.exit);
+			sinon.assert.called(DummyHandlers.mainSectionHandler.exit);
+			sinon.assert.called(DummyHandlers.indexHandler.update);
+			sinon.assert.called(DummyHandlers.mainSectionHandler.enter);
+			sinon.assert.called(DummyHandlers.subSectionHandler.enter);
+		});
+	});
+
+	it("should throw an Error if a route change is raised while one is being processed", function (done) {
+		var reject = sinon.spy();
+
+		AppRouter.go("/foo/bar");
+
+		waits(100);
+
+		runs(function() {
+			AppRouter.go("/foo").then(null, reject).catch(reject);
+		});
+
+		waits(100);
+
+		runs(function() {
+			sinon.assert.calledOnce(reject);
+		});
+	});
+});
 `;
 
 var output = babel.transform(code, {
